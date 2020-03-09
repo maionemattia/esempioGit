8a452b62ebe5345a682bc3017967dbb29412a42d
mattiamaione <mattiamaione@macbook-pro>
2020-03-09 17:59:15 +0100

    Commento di prova

--- src/main/java/it/polito/tdp/esempioGit/EntryPoint.java
+++ src/main/java/it/polito/tdp/esempioGit/EntryPoint.java
@@ -21,6 +21,8 @@ public class EntryPoint extends Application {
         stage.setScene(scene);
         stage.show();
     }
+    
+    // Altro commento ancora piu inutile
 
     /**
      * The main() method is ignored in correctly deployed JavaFX application.
